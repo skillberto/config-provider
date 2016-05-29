2fc45690caed6ef54f1eabf0815c0bbc3a53a1b3
Norbert Heiszler <heiszler.norbert@gmail.com>
2016-05-29 20:40:43 +0200

    init usage

---
+++ ConfigProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Skillberto\Provider\Config;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class ConfigProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+
+    }
+}
